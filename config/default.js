6cd28748168c89201a39015e8772fafa6a867d65
waruna <waruna@duosoftware.com>
2019-05-23 11:27:23 +0530

    add port

--- config/default.js
+++ config/default.js
@@ -43,8 +43,7 @@ module.exports = {
             "longtermtokenduration":2592000, /*in secounds*/
             "vdomain": "localhost",
             "domain": "localhost",
-            "internalport": "6689",
-            "externalport": "6690",
+            "port": "3000",
             "version": "1.0.0.0",
             //"messenger": "user",
             "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
