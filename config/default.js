3870f152badb6a12f4d4530e4ec43283a9ddf891
waruna <waruna@duosoftware.com>
2019-05-23 11:29:37 +0530

    config change for testing

--- config/default.js
+++ config/default.js
@@ -51,7 +51,7 @@ module.exports = {
 
         },
     "Services": {
-        "call_back_url": "ad6f1324.ngrok.io",
+        "call_back_url": "104.236.197.119",
         "call_back_url_port": '3637',
         "call_back_url_version":"1.0.0.0",
 
