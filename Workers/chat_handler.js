b4b72cd39d1ae77980ec26f94d84618f63018d5f
Anjali <anjalipremachandra@gmail.com>
2025-09-12 13:37:07 +0530

    added session id

--- Workers/chat_handler.js
+++ Workers/chat_handler.js
@@ -117,7 +117,10 @@ function init_and_inform_to_agent(resource, tenantId, companyId) {
                 } else {
                     var msg_data = JSON.parse(sessiondata).client_data;
                     msg_data.Skills = resource.Skills;
-                  
+                    msg_data.SessionID = resource.SessionID;
+                    console.log("msg_data", msg_data);
+                    
+                    
                     
                  return   socket_handler.send_message_agent(resource.ResourceInfo.Profile, 'client', msg_data).then(function (value) {
                         console.log("**** Agent Found and informed to agent ****",value);
