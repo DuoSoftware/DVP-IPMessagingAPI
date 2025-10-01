c499a7d4caacb97de7d3c101309f21c913143330
Anjali <anjalipremachandra@gmail.com>
2025-10-01 12:01:33 +0530

    added code

--- Workers/socket_connect_handler.js
+++ Workers/socket_connect_handler.js
@@ -396,7 +396,7 @@ module.exports.send_message_agent = function(agent, eventName, message) {
                 PersonalMessage.find(query)
                     .lean()
                     .sort({ created_at: -1 })
-                    .limit(5)
+                    .limit(50)
                     .then(latestmessages => {
                         console.log("Mongo latestmessages:", latestmessages);
                          if (latestmessages && Array.isArray(latestmessages)) {
