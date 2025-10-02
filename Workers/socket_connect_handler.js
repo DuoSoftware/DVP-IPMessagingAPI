cb9bfd68343184589faa99fc24b33913ba8899e0
Anjali <anjalipremachandra@gmail.com>
2025-10-02 11:47:36 +0530

    remove condition

--- Workers/socket_connect_handler.js
+++ Workers/socket_connect_handler.js
@@ -366,17 +366,16 @@ module.exports.send_message_agent = function(agent, eventName, message) {
                     status: "pending",  
                     uuid: id,  
                     message: message.data || message.message || "accept Request",  // Message content
-                    data: message.data || message.message || "ccept Request",  // Message content
                     channel: message.channel || "default",  
                     wa_id: message.jti ,  // WhatsApp ID or similar identifier
                     session: message.sessionId,  
                     from: message.from,  
                     to: message.to, 
                     direction: "inbound",  
-                    agentId: message.name || message.from,  // Agent ID, default to 'to' if missing
-                    agentName: message.agentName || message.from,  // Agent name, default to from if missing
+                    agentId: message.name || message.to,  // Agent ID, default to 'to' if missing
+                    agentName: message.agentName || message.to,  // Agent name, default to from if missing
                     jti: message.jti || "",  // JWT token ID (if any)
-                    externalUserId: message.externalUserId || "",  // External user ID (if any)
+                    externalUserId: message.externalUserId ,  // External user ID (if any)
                     company: message.company,  // Company ID
                     tenant: message.tenant,  // Tenant ID
                     BusinessUnit: message.BusinessUnit || "default",  // Business unit
