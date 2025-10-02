c5d32ca15af3303dab535e18d1882d87cadd6251
Anjali <anjalipremachandra@gmail.com>
2025-10-02 17:30:28 +0530

    change mongo connection configuration

--- Workers/mongo_handler.js
+++ Workers/mongo_handler.js
@@ -12,7 +12,24 @@ var mongoPass = config.Mongo.password;
 var mongoreplicaset= config.Mongo.replicaset;
 var  mongoType = config.Mongo.type || "mongodb";
 
-const mongoURI = `${mongoType}://${mongoUser}:${encodeURIComponent(mongoPass)}@${mongoIP}:${mongoPort}/${dbName}`;
+//const mongoURI = `${mongoType}://${mongoUser}:${encodeURIComponent(mongoPass)}@${mongoIP}:${mongoPort}/${dbName}`;
+
+let connectionString = '';
+if (mongoIP.length > 1) {
+    const hosts = mongoIP.map(ip => `${ip}:${mongoPort}`).join(',');
+    connectionString = `mongodb://${mongoUser}:${mongoPass}@${hosts}/${dbName}`;
+    if (mongoreplicaset) {
+        connectionString += `?replicaSet=${mongoreplicaset}`;
+    }
+} else {
+    connectionString = `mongodb://${mongoUser}:${mongoPass}@${mongoIP[0]}:${mongoPort}/${dbName}`;
+}
+
+console.log("MongoDB Connection String:", connectionString);
+
+
+
+
 
 mongoose.connection.on("connected", () => {
     console.log("✅ MongoDB connected");
@@ -30,8 +47,8 @@ mongoose.connection.on("reconnected", () => {
     console.log("🔄 MongoDB reconnected");
 });
 
-// Connect once at startup
-mongoose.connect(mongoURI, {
+
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
